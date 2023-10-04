010a539b170ac1cbf8fca6df3107b9b406a47d36
Fred <fsolleder3@gmail.com>
2023-10-04 08:37:02 -0700

    uhhhhhhhhhhhhhhhh no image for rn :D

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,69 @@
+function showWL() {
+  document.querySelector(".open-watchlist").style.display = "none";
+  document.querySelector(".watchlist-containerMd").style.display = "block";
+}
+
+function hideWL() {
+  document.querySelector(".open-watchlist").style.display = "block";
+  document.querySelector(".watchlist-containerMd").style.display = "none";
+}
+
 const API_KEY = '47d7119b1ceef15064fd4cc997c6f5bb';
 const BASE_URL = 'https://api.themoviedb.org/3'
 
+
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0N2Q3MTE5YjFjZWVmMTUwNjRmZDRjYzk5N2M2ZjViYiIsInN1YiI6IjY1MWIxOGE2ZWE4NGM3MDBlYjlhNTdiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rCk2cFWbH8KUntzb4n457mmW7rG73VSOxMMd8gAOrLc'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0N2Q3MTE5YjFjZWVmMTUwNjRmZDRjYzk5N2M2ZjViYiIsInN1YiI6IjY1MWIxOGE2ZWE4NGM3MDBlYjlhNTdiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rCk2cFWbH8KUntzb4n457mmW7rG73VSOxMMd8gAOrLc",
+  },
 };
 
-document.addEventListener('DOMContentLoaded', function(){
-
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
-  .then(response => response.json())
-  .then(data => {
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
       const movies = data.results;
-      const cards = document.querySelectorAll('.card');
+      const cards = document.querySelectorAll(".card");
 
       for (let i = 0; i < cards.length; i++) {
-          if (movies[i]) {
-              const movie = movies[i];
-              cards[i].querySelector('.movie-poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-              cards[i].querySelector('.movie-title').innerText = movie.title;
-              cards[i].querySelector('.movie-description').innerText = movie.overview;
+        if (movies[i]) {
+          const movie = movies[i];
+          cards[i].querySelector(".movie-poster").src =
+            "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+          cards[i].querySelector(".movie-title").innerText = movie.title;
+          cards[i].querySelector(".movie-description").innerText =
+            movie.overview;
 
-              const movieLink = cards[i].querySelector('.movie-link');
-              movieLink.href = `/movie-details.html?id=${movie.id}`;
-          }
+          const movieLink = cards[i].querySelector(".movie-link");
+          movieLink.href = `/movie-details.html?id=${movie.id}`;
+        }
       }
-  })
-  .catch(err => console.error(err));
+    })
+    .catch((err) => console.error(err));
 
-
-  if(document.querySelector('.movie-poster')) {
+  if (document.querySelector(".movie-poster")) {
     const urlParam = new URLSearchParams(window.location.search);
-    const movieID = urlParam.get('id');
+    const movieID = urlParam.get("id");
 
     if (movieID) {
       fetch(`https://api.themoviedb.org/3/movie/${movieID}`, options)
-        .then(response => response.json())
-        .then(movie => {
-          document.querySelector('.movie-poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-          document.querySelector('.movie-title').innerText = movie.title;
-          document.querySelector('.movie-description').innerText = movie.overview;
+
+        .then((response) => response.json())
+        .then((movie) => {
+          document.querySelector(".movie-poster").src =
+            "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+          document.querySelector(".movie-title").innerText = movie.title;
+          document.querySelector(".movie-description").innerText =
+            movie.overview;
         });
     }
-
-    function displayResults(movies) {
-      const cards = document.querySelectorAll('.card');
-
-      movies.slice(0, cards.length).forEach((movie, index) => {
-        cards[index].querySelector('.movie-poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
-        cards[index].querySelector('.movie-title').innerText = movie.title;
-        cards[index].querySelector('.movie-description').innerText = movie.overview;
-
-        const movieLink = cards[index].querySelector('.movie-link')
-        movieLink.href = `/movie-details.html?id=${movie.id}`
-      });
-    }
   }
-
   document.getElementById('searchBar').addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
       const query = event.target.value;
@@ -80,5 +84,19 @@ document.addEventListener('DOMContentLoaded', function(){
     .catch(error => {
       console.error('Error: ' + error)
     })
+  }
+
+  
+  function displayResults(movies) {
+    const cards = document.querySelectorAll('.card');
+
+    movies.slice(0, cards.length).forEach((movie, index) => {
+      cards[index].querySelector('.movie-poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+      cards[index].querySelector('.movie-title').innerText = movie.title;
+      cards[index].querySelector('.movie-description').innerText = movie.overview;
+
+      const movieLink = cards[index].querySelector('.movie-link')
+      movieLink.href = `/movie-details.html?id=${movie.id}`
+    });
   }
 });
